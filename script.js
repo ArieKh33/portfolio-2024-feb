5f97870beab8b1da092eba3cedd4b6d1f2edd442
Arie Kharamany <arie.kharamany@baton-software.nl>
2023-08-21 11:42:39 +0200

    font en foto weg

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const htmlButton = () => {
     if(!text) {return false; }
     if(!logo) {return false;}
     button.innerHTML = "My experiences with: HTML5";
-    text.innerHTML = "test html5";
+    text.innerHTML = "No website exists without HTML and as my introductory language to programming I have to say that I enjoy working with it a lot since it's simple and very intuative.";
     logo.src = "html-logo.svg";
     button.className = "htmlActive";
 };
@@ -21,7 +21,7 @@ const cssButton = () => {
     if(!text) {return false; }
     if(!logo) {return false;}
     button.innerHTML = "My experiences with: CSS3";
-    text.innerHTML = "test CCS3";
+    text.innerHTML = "Unlike HTML, CSS isn't mandatory but it definitely will make your website stand out more, and how I work with css is to make every website as simple and clear as I possibly can so that everyone from a programming genius to the elderly can understand what the website is about.";
     logo.src = "css3-logo.svg";
     button.className = "css3Active";
 };
@@ -34,7 +34,7 @@ const phpButton = () => {
     if(!text) {return false; }
     if(!logo) {return false;}
     button.innerHTML = "My experiences with: PHP";
-    text.innerHTML = "test PHP";
+    text.innerHTML = "After all that web development some variety always welcome and PHP is first back-end language that I work with, it gave me a nice introduction into how fun it can be to work on databases. I am still a novice on this language compared to JavaScript but I have to admit that I always love the challenge.";
     logo.src = "php-logo.svg";
     button.className = "phpActive";
 };
@@ -47,7 +47,7 @@ const javascriptButton = () => {
     if(!text) {return false; }
     if(!logo) {return false;}
     button.innerHTML = "My experiences with: JavaScript";
-    text.innerHTML = "test JavaScript";
+    text.innerHTML = "only working with HTML and CSS to create websites is fun but quite limiting and there's where JavaScript comes in, I would have to say it's a language that I didn't like at first but learning more and more about it made me realize how important and interesting of a language that it is.";
     logo.src = "javascript-logo.svg";
     button.className = "javascriptActive";
 };
@@ -60,7 +60,7 @@ const bootstrapButton = () => {
     if(!text) {return false; }
     if(!logo) {return false;}
     button.innerHTML = "My experiences with: Bootstrap";
-    text.innerHTML = "test Bootstrap";
+    text.innerHTML = "My preference at first was to hard code everything, but after starting to learn about the framework bootstrap I have changed my mind. I still have a preference to writing everything myself, but I've learnt that it can be very fun and useful to learn some frameworks on the side too.";
     logo.src = "bootstrap-logo.svg";
     button.className = "bootstrapActive";
 };
