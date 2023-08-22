4b0d5566f731d3a98e0739557d71cfaaaaf32987
Arie Kharamany <arie.kharamany@baton-software.nl>
2023-08-22 13:48:12 +0200

    typescript conversion

--- script.ts
+++ script.ts
@@ -1,98 +1,77 @@
-var htmlButton = function () {
-    var button = document.getElementById("titleLanguage");
-    var text = document.getElementById("languageExplain");
-    var logo = document.getElementById("language_logo");
-    if (!button) {
-        return false;
-    }
-    if (!text) {
-        return false;
-    }
-    if (!logo) {
-        return false;
-    }
+
+const htmlButton = () => {
+    const button = document.getElementById("titleLanguage");
+    const text = document.getElementById("languageExplain");
+    const logo = document.getElementById("language_logo") as HTMLImageElement;
+    if(!button) { return false; }
+    if(!text) {return false; }
+    if(!logo) {return false;}
     button.innerHTML = "My experiences with: HTML5";
     text.innerHTML = "No website exists without HTML and as my introductory language to programming I have to say that I enjoy working with it a lot since it's simple and very intuative.";
     logo.src = "html-logo.svg";
     button.className = "htmlActive";
 };
-var cssButton = function () {
-    var button = document.getElementById("titleLanguage");
-    var text = document.getElementById("languageExplain");
-    var logo = document.getElementById("language_logo");
-    if (!button) {
-        return false;
-    }
-    if (!text) {
-        return false;
-    }
-    if (!logo) {
-        return false;
-    }
+
+
+const cssButton = () => {
+    const button = document.getElementById("titleLanguage");
+    const text = document.getElementById("languageExplain");
+    const logo = document.getElementById("language_logo") as HTMLImageElement;
+    if(!button) { return false; }
+    if(!text) {return false; }
+    if(!logo) {return false;}
     button.innerHTML = "My experiences with: CSS3";
     text.innerHTML = "Unlike HTML, CSS isn't mandatory but it definitely will make your website stand out more, and how I work with css is to make every website as simple and clear as I possibly can so that everyone from a programming genius to the elderly can understand what the website is about.";
     logo.src = "css3-logo.svg";
     button.className = "css3Active";
 };
-var phpButton = function () {
-    var button = document.getElementById("titleLanguage");
-    var text = document.getElementById("languageExplain");
-    var logo = document.getElementById("language_logo");
-    if (!button) {
-        return false;
-    }
-    if (!text) {
-        return false;
-    }
-    if (!logo) {
-        return false;
-    }
+
+const phpButton = () => {
+    const button = document.getElementById("titleLanguage");
+    const text = document.getElementById("languageExplain");
+    const logo = document.getElementById("language_logo") as HTMLImageElement;
+    if(!button) { return false; }
+    if(!text) {return false; }
+    if(!logo) {return false;}
     button.innerHTML = "My experiences with: PHP";
     text.innerHTML = "After all that web development some variety always welcome and PHP is first back-end language that I work with, it gave me a nice introduction into how fun it can be to work on databases. I am still a novice on this language compared to JavaScript but I have to admit that I always love the challenge.";
     logo.src = "php-logo.svg";
     button.className = "phpActive";
 };
-var javascriptButton = function () {
-    var button = document.getElementById("titleLanguage");
-    var text = document.getElementById("languageExplain");
-    var logo = document.getElementById("language_logo");
-    if (!button) {
-        return false;
-    }
-    if (!text) {
-        return false;
-    }
-    if (!logo) {
-        return false;
-    }
+
+const javascriptButton = () => {
+    const button = document.getElementById("titleLanguage");
+    const text = document.getElementById("languageExplain");
+    const logo = document.getElementById("language_logo") as HTMLImageElement;
+    if(!button) { return false; }
+    if(!text) {return false; }
+    if(!logo) {return false;}
     button.innerHTML = "My experiences with: JavaScript";
     text.innerHTML = "only working with HTML and CSS to create websites is fun but quite limiting and there's where JavaScript comes in, I would have to say it's a language that I didn't like at first but learning more and more about it made me realize how important and interesting of a language that it is.";
     logo.src = "javascript-logo.svg";
     button.className = "javascriptActive";
 };
-var bootstrapButton = function () {
-    var button = document.getElementById("titleLanguage");
-    var text = document.getElementById("languageExplain");
-    var logo = document.getElementById("language_logo");
-    if (!button) {
-        return false;
-    }
-    if (!text) {
-        return false;
-    }
-    if (!logo) {
-        return false;
-    }
+
+const bootstrapButton = () => {
+    const button = document.getElementById("titleLanguage");
+    const text = document.getElementById("languageExplain");
+    const logo = document.getElementById("language_logo") as HTMLImageElement;
+    if(!button) { return false; }
+    if(!text) {return false; }
+    if(!logo) {return false;}
     button.innerHTML = "My experiences with: Bootstrap";
     text.innerHTML = "My preference at first was to hard code everything, but after starting to learn about the framework bootstrap I have changed my mind. I still have a preference to writing everything myself, but I've learnt that it can be very fun and useful to learn some frameworks on the side too.";
     logo.src = "bootstrap-logo.svg";
     button.className = "bootstrapActive";
 };
-var html5Button = document.getElementsByClassName("language_button_HTML5")[0];
-var css3Button = document.getElementsByClassName("language_button_CSS3")[0];
-var PHPButton = document.getElementsByClassName("language_button_PHP")[0];
-var JavaScriptButton = document.getElementsByClassName("language_button_JavaScript")[0];
-var BootstrapButton = document.getElementsByClassName("language_button_Bootstrap")[0];
+
+
+const html5Button = document.getElementsByClassName("language_button_HTML5")[0];
+const css3Button = document.getElementsByClassName("language_button_CSS3")[0];
+const PHPButton = document.getElementsByClassName("language_button_PHP")[0];
+const JavaScriptButton = document.getElementsByClassName("language_button_JavaScript")[0];
+const BootstrapButton = document.getElementsByClassName("language_button_Bootstrap")[0];
+
 html5Button.addEventListener("click", htmlButton);
 css3Button.addEventListener("click", cssButton);
 PHPButton.addEventListener("click", phpButton);
