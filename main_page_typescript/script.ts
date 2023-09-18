bc2794c9fb5faefef13ac240239a82b14dff0fa5
Arie Kharamany <arie.kharamany@baton-software.nl>
2023-09-18 13:55:01 +0200

    introduction changed index and project 2

--- main_page_typescript/script.ts
+++ main_page_typescript/script.ts
@@ -20,13 +20,13 @@ const data = [
        {
         id: "JavaScript",
         title: "My experiences with: JavaScript",
-        description: "Only working with HTML and CSS to create websites is fun and all but it leaves a lot to be desired, so when I found out about the endless possibilities with JavaScript I was delighted and have to say that it is definitely one of the easier and more fun languages to use.",
+        description: "Only working with HTML and CSS to create websites is fun and all but it leaves a lot to be desired, so when I found out about the endless possibilities with JavaScript I was delighted to see that it is definitely one of the easier and more fun languages to use.",
         logo: "/main_page_language_logos/javascript-logo.svg"
     },
        {
         id: "Bootstrap",
         title: "My experiences with: Bootstrap",
-        description: "My preference at first was to hard code everything, and to a point it is still very important to learn the fundamentals, but after looking around I found bootstrap and I have to say that it is an interesting experience. It took some time getting used to but it gave me some wonderful insight on how frameworks are.",
+        description: "My preference at first was to hard code everything, and to a point it is still very important to learn the fundamentals, but after looking around I found bootstrap and I have to say that it is an interesting experience to work with. It was a nice and simple introduction on how frameworks work.",
         logo: "/main_page_language_logos/bootstrap-logo.svg"
     },
 ]
